d78499eb43d0116290bfe7236fd14ef120cf9ea5
Anton Mavrin <mavrin@outlook.com>
2020-03-31 22:46:39 +0300

    Lesson-02 finished

--- app/src/main/java/tech/k0tlin/smack/MainActivity.kt
+++ app/src/main/java/tech/k0tlin/smack/MainActivity.kt
@@ -3,8 +3,6 @@ package tech.k0tlin.smack
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,7 +12,6 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -59,7 +56,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendMsgBtnClicked(view: View) {
-        
+
     }
 
 }
